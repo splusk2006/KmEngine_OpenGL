d170866a297c5a89c4777894c73aaca1461a8a44
Kimin Kim <51414960+splusk2006@users.noreply.github.com>
2019-06-06 03:03:38 +0900

    Added [glfw3,glad]

---
+++ KmEngine_OpenGL/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+
+int main(int argc, char* argv[])
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	return 0;
+}
